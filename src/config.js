66398aacebdb5063665d4ba2c60ab2079479c251
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2022-10-04 12:27:05 +0530

    change server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	api_url: "https://s-hub.onrender.com",
+	api_url: "https://shub-server.vercel.app",
 	//s://s-hub.onrender.com
 	ds_app_client: "1022424500916195349",
 	gh_app_client: "9e4acaaf6d4753b453bd",
